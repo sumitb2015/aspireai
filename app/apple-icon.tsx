3819532d96994778dc52732c2738d7e93ff87950
sumitb2015 <sumitb2015@users.noreply.github.com>
2026-04-26 10:20:06 +0530

    feat: add dynamic favicon and apple-icon using next/og

---
+++ app/apple-icon.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from 'next/og'
+ 
+export const runtime = 'edge'
+ 
+export const size = {
+  width: 180,
+  height: 180,
+}
+export const contentType = 'image/png'
+ 
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 120,
+          background: '#0070f3',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: '40px',
+          fontWeight: 900,
+          fontStyle: 'italic',
+        }}
+      >
+        A
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
